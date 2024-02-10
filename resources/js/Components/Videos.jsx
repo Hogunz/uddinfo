5067f5e737465dc47fa14246df8b66dd0dd9f37a
Hogunz <cjdc1414@gmail.com>
2024-02-11 00:50:29 +0800

    2/11/2024

--- resources/js/Components/Videos.jsx
+++ resources/js/Components/Videos.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useRef } from "react";
+
 export default function Video() {
+    //Carousel
+    //Videos
     const [isPlaying, setIsPlaying] = useState(false);
     const videoRef = useRef(null);
 
@@ -18,32 +21,38 @@ export default function Video() {
     };
 
     return (
-        <div className="video-player flex justify-center mb-8">
-            <div className="relative">
-                <video
-                    ref={videoRef}
-                    className="max-w-7xl rounded-lg "
-                    autoPlay={true}
-                    controls={isPlaying}
-                    onClick={handlePlay}
-                    onPlay={handleVideoStart}
-                    onPause={() => setIsPlaying(false)}
-                >
-                    <source src="vid/udd-history.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-                {!isPlaying && (
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <button
-                            className="relative flex items-center justify-center w-16 h-16 bg-blue-600 rounded-full shadow-md hover:shadow-lg font-syne"
-                            onClick={handlePlay}
-                        >
-                            Play
-                            <span className="absolute inline-flex h-12 w-12 bg-blue-400 rounded-full animate-ping"></span>
-                        </button>
-                    </div>
-                )}
+        <>
+            <div
+                id="video-player"
+                className="video-player flex justify-center mb-8"
+            >
+                <div className="relative">
+                    <video
+                        ref={videoRef}
+                        className="max-w-7xl rounded-lg "
+                        autoPlay={true}
+                        loop={true}
+                        controls={isPlaying}
+                        onClick={handlePlay}
+                        onPlay={handleVideoStart}
+                        onPause={() => setIsPlaying(false)}
+                    >
+                        <source src="vid/udd-history.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    {!isPlaying && (
+                        <div className="absolute inset-0 flex items-center justify-center">
+                            <button
+                                className="relative flex items-center justify-center w-16 h-16 bg-blue-600 rounded-full shadow-md hover:shadow-lg font-syne"
+                                onClick={handlePlay}
+                            >
+                                Play
+                                <span className="absolute inline-flex h-12 w-12 bg-blue-400 rounded-full animate-ping"></span>
+                            </button>
+                        </div>
+                    )}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
